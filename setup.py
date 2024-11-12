df2205a8904dec0185e755cd0531ea8030c0bf80
Anarchist4 <kashanabdullah19@gmail.com>
2024-11-12 16:46:46 +0500

    folder structure created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Gen_AI Project",
+    version = '0.0.0',
+    author = "Kashan Abdullah",
+    author_email = "kashanabdullah19@gmail.com",
+    packages = find_packages(),
+    install_requires = [] 
+    
+)
